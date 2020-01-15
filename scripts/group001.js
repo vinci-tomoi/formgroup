536196623f710f0259040198bd5d483ae41bf75f
vinci-tomoi <kanako.tomoi@i-vinci.co.jp>
2020-01-15 13:45:56 +0900

    Add: グループ追加、削除機能の追加

--- scripts/group001.js
+++ scripts/group001.js
@@ -1,18 +1,33 @@
 $(function() {
 
-  $('#adduser').click(function() {
-    $(this).before('<p id="name_u">ユーザ名<input type="text" name="name_u"><input type="button" value="×" id="removeuser"></p>');
-  });
-
-  // $('#removeuser').click(function() {
-  //   $('#name_u').remove();
+  // $('#adduser').click(function() {
+  //   $(this).before('<p id="user">ユーザ名<input type="text" name="name_u" class="name_u"><input type="button" value="×" id="removeuser" class="remove"></p>');
   // });
+
+  $(document).on('click', '#adduser', function() {
+    
+    $(this).before('<p id="user">ユーザ名<input type="text" name="name_u" class="name_u"><input type="button" value="×" id="removeuser" class="remove"></p>');
+
+  })
 
   $(document).on('click', '#removeuser', function() {
     
-    $('#name_u').remove();
+    $('#user').remove();
 
-})
+  })
+
+
+  $('#addgroup').click(function() {
+    $(this).before('<div id="group">グループ名<input type="text" name="name_g" class="name_g"><p id="user">ユーザ名<input type="text" name="name_u" class="name_u"><input type="button" value="×" id="removeuser" class="remove"></p><input type="button" value="+" id="adduser"><div id="removegroup"><input type="button" value="×" class="remove"></div></div>');
+  });
+
+  $(document).on('click', '#removegroup', function() {
+    
+    $('#group').remove();
+
+  })
+
+
   
 
 });
