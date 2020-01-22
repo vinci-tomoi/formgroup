8b15eaaae40626279335fff544e3af943f2f7643
vinci-tomoi <kanako.tomoi@i-vinci.co.jp>
2020-01-22 15:34:39 +0900

    Add: 外部JSONファイルの読み込みと反映

--- scripts/group002.js
+++ scripts/group002.js
@@ -14,7 +14,7 @@ $(function() {
 
 
   $('#addgroup').click(function() {
-    $(this).before('<form id="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop"><p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"><input type="button" value="×" id="removeuser" class="remove"></p><input type="button" value="+" id="adduser"><div id="removegroup"><input type="button" value="×" class="remove"></div></form>');
+    $(this).before('<form id="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop"><div id="users"><p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"><input type="button" value="×" id="removeuser" class="remove"></p></div><input type="button" value="+" id="adduser"><div id="removegroup"><input type="button" value="×" class="remove"></div></form>');
   });
 
   $(document).on('click', '#removegroup', function() {
@@ -77,9 +77,7 @@ $(function() {
       var readJson = JSON.parse(reader.result);
       console.log(readJson);
 
-
       $.each(readJson, function(index, val) {
-        console.log(val);
 
         if (index === 0) {
           $('#name_g').val(val.name);
@@ -89,13 +87,33 @@ $(function() {
             if (i === 0) {
               $('#name_u').val(val.users[0]);
             } else {
-              $('#user').after('<p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"><input type="button" value="×" id="removeuser" class="remove"></p>');
+              $('#groups > form:first').find('#users').append('<p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite"><input type="button" value="×" id="removeuser" class="remove"></p>');
+              $("input[value='rewrite']").attr('value', val.users[i]);
+              console.log(val.users[i]);
+              // $('#groups > form:first').find('#users').css('color', '#FF0000');
+            }
+
+          }
+
+
+        } else {
+
+          $('#groups').append('<form id="group" name="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop" value="rewrite"><div id="users"><p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite2"><input type="button" value="×" id="removeuser" class="remove"></p></div><input type="button" value="+" id="adduser"><div id="removegroup"><input type="button" value="×" class="remove"></div></form>');
+
+          $("input[value='rewrite']").attr('value', val.name);
+
+          for (var i = 0; i < val.users.length; i++) {
+
+            if (i === 0) {
+              $("input[value='rewrite2']").attr('value', val.users[0]);
+            } else {
+              $('#groups > form:last').find('#users').append('<p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite"><input type="button" value="×" id="removeuser" class="remove"></p>');
+              $("input[value='rewrite']").attr('value', val.users[i]);
               console.log(val.users[i]);
             }
 
           }
 
-          console.log(val.name);
         }
 
       });
