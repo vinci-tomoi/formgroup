8ce79e837f22cd9f936591b8578def5d112775e6
vinci-tomoi <kanako.tomoi@i-vinci.co.jp>
2020-01-23 17:38:07 +0900

    Add: グループ数およびユーザー数が1のときに削除ボタンを表示しない機能

--- scripts/group002.js
+++ scripts/group002.js
@@ -1,27 +1,65 @@
 $(function() {
 
+  var input_u = '<p id="user" class="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"><input type="button" value="×" id="removeuser" class="remove"></p>';
+
+  var input_g = '<form name="group" id="group" class="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop"><div id="users" class="users"><p id="user" class="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"></p></div><input type="button" value="+" id="adduser"><div id="removegroup" class="removegroup"><input type="button" value="×" class="remove_g"></div></form>';
+
   $(document).on('click', '#adduser', function() {
+
+    var count = $(this).prev().children().length;
+
+    if (count === 1) {
+      $(this).prev().children('p:first').append('<input type="button" value="×" id="removeuser" class="remove"></input>');
+    }
+   
+
+    $(this).prev().append(input_u);
+
     
-    $(this).before('<p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"><input type="button" value="×" id="removeuser" class="remove"></p>');
 
   })
 
-  $(document).on('click', '#removeuser', function() {
-    
+  $(document).on('click', '.remove', function() {
+
+    var count = $(this).parents('.users').find('.user').length;
+
+    if (count === 2) {
+      $(this).parent().siblings().find('input:last').remove();
+    }
+
     $(this).parent().remove();
 
   })
 
 
-  $('#addgroup').click(function() {
-    $(this).before('<form id="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop"><div id="users"><p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop"><input type="button" value="×" id="removeuser" class="remove"></p></div><input type="button" value="+" id="adduser"><div id="removegroup"><input type="button" value="×" class="remove"></div></form>');
-  });
+  $(document).on('click', '#addgroup', function() {
 
-  $(document).on('click', '#removegroup', function() {
+    var count = $(this).prev().children().length;
+
+    if (count === 1) {
+      $(this).prev().children('form:first').append('<div id="removegroup" class="removegroup"><input type="button" value="×" class="remove_g"></div>');
+    }
+   
+
+    $(this).prev().append(input_g);
+
     
-    $('#group').remove();
 
   })
+
+
+  $(document).on('click', '.removegroup', function() {
+
+    var count = $(this).parents('.groups').find('.group').length;
+
+    if (count === 2) {
+      $(this).parent().siblings().find('div:last').remove();
+    }
+
+    $(this).parent().remove();
+
+  })
+  
 
   $('#save').click(() => {
     
@@ -77,7 +115,7 @@ $(function() {
             if (i === 0) {
               $('#name_u').val(val.users[0]);
             } else {
-              $('#groups > form:first').find('#users').append('<p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite"><input type="button" value="×" id="removeuser" class="remove"></p>');
+              $('#groups > form:first').find('#users').append('<p id="user" class="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite"><input type="button" value="×" id="removeuser" class="remove"></p>');
               $("input[value='rewrite']").attr('value', val.users[i]);
               console.log(val.users[i]);
               // $('#groups > form:first').find('#users').css('color', '#FF0000');
@@ -88,7 +126,7 @@ $(function() {
 
         } else {
 
-          $('#groups').append('<form id="group" name="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop" value="rewrite"><div id="users"><p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite2"><input type="button" value="×" id="removeuser" class="remove"></p></div><input type="button" value="+" id="adduser"><div id="removegroup"><input type="button" value="×" class="remove"></div></form>');
+          $('#groups').append('<form id="group" name="group" class="group"><label for="name_g">グループ名</label><input type="text" name="name_g"  id="name_g" class="name_g" form="fileop" value="rewrite"><div id="users"><p id="user" class="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite2"><input type="button" value="×" id="removeuser" class="remove"></p></div><input type="button" value="+" id="adduser"><div id="removegroup" class="removegroup"><input type="button" value="×" class="remove"></div></form>');
 
           $("input[value='rewrite']").attr('value', val.name);
 
@@ -97,7 +135,7 @@ $(function() {
             if (i === 0) {
               $("input[value='rewrite2']").attr('value', val.users[0]);
             } else {
-              $('#groups > form:last').find('#users').append('<p id="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite"><input type="button" value="×" id="removeuser" class="remove"></p>');
+              $('#groups > form:last').find('#users').append('<p id="user" class="user"><label for="name_u">ユーザ名</label><input type="text" name="name_u" id="name_u" class="name_u" form="fileop" value="rewrite"><input type="button" value="×" id="removeuser" class="remove"></p>');
               $("input[value='rewrite']").attr('value', val.users[i]);
               console.log(val.users[i]);
             }
